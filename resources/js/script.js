5c8280f4617a61d0d97c1f9a83e506fef1a0d20a
RensWest <rens.westerveld@outlook.com>
2022-12-07 15:55:40 +0100

    Toch?

---
+++ resources/js/script.js
@@ -0,0 +1,39 @@
+// var setTimeout;
+
+//         setTimeout(function() {
+//             location.href = "index.html";
+//         }, 3000);
+
+
+var button1 = document.querySelector('#button_1');
+var button2 = document.querySelector('#button_2');
+var button3 = document.querySelector('#button_3');
+var button4 = document.querySelector('#button_4');
+var video = document.querySelector('#video');
+
+
+button1.addEventListener('click', speel1);
+
+function speel1() {
+    video.src = "./resources/img/animation1.mp4";
+}
+
+
+button2.addEventListener('click', speel2);
+
+function speel2() {
+    video.src = "./resources/img/animation2.mp4";
+}
+
+
+button3.addEventListener('click', speel3);
+
+function speel3() {
+    video.src = "./resources/img/animation3.mp4";
+}
+
+button4.addEventListener('click', speel4);
+
+function speel4() {
+    video.src = "./resources/img/karakteridle.mp4";
+}
